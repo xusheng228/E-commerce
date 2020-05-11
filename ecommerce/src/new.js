d0d8bc35a454928641b4af618180169cab619412
xusheng228 <xusheng228@gmail.com>
2020-05-11 11:42:20 -0400

    this is a sample javascriot file

---
+++ ecommerce/src/new.js
@@ -0,0 +1,14 @@
+
+   function linearSearch(arr, a) {
+  for (var i = 0; i <= arr.length; i++) {
+    if (arr[i] === a) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([1, 3, 45, 67, 78], 32));
+//output: -1;
+
+console.log(linearSearch([1, 3, 45, 67, 78], 78));
+//output: 4
+
