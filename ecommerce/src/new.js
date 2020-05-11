7ddeeb17b74a2b99338c2c5e1403ca9a97146879
xusheng228 <xusheng228@gmail.com>
2020-05-11 11:49:33 -0400

    formatted

--- ecommerce/src/new.js
+++ ecommerce/src/new.js
@@ -1,9 +1,8 @@
-
-   function linearSearch(arr, a) {
-  for (var i = 0; i <= arr.length; i++) {
-    if (arr[i] === a) return i;
+    function linearSearch(arr, a) {
+      for (var i = 0; i <= arr.length; i++) {
+        if (arr[i] === a) return i;
   }
-  return -1;
+          return -1;
 }
 
 console.log(linearSearch([1, 3, 45, 67, 78], 32));
